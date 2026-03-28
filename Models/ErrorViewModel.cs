ecdfbde27f171efa47d01d9b277255b55b093627
Fernando Lezama <ferlezama0930@gmail.com>
2026-03-28 13:41:44 -0500

    crear proyecto mvc base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PortalCampanasComerciales.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
